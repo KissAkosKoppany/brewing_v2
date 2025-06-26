5482c253a9672c76c6a148ef7360f8e6686a7924
KissAkos <kopyherceg@gmail.com>
2025-06-26 20:01:52 +0300

    basic style for shop item cards

--- brewing_v2/src/Routes/Shop/Components/ShopItems.jsx
+++ brewing_v2/src/Routes/Shop/Components/ShopItems.jsx
@@ -10,6 +10,48 @@ const ShopItems = () => {
             <h1>Beer name</h1>
             <p>Beer description</p>
         </div>
+        <div className='shop-item-card'>
+            <div className='card-image-container'>
+                <img src="images/neko.webp" alt="neko" />
+            </div>
+            <h1>Beer name</h1>
+            <p>Beer description</p>
+        </div>
+        <div className='shop-item-card'>
+            <div className='card-image-container'>
+                <img src="images/neko.webp" alt="neko" />
+            </div>
+            <h1>Beer name</h1>
+            <p>Beer description</p>
+        </div>
+        <div className='shop-item-card'>
+            <div className='card-image-container'>
+                <img src="images/neko.webp" alt="neko" />
+            </div>
+            <h1>Beer name</h1>
+            <p>Beer description</p>
+        </div>
+        <div className='shop-item-card'>
+            <div className='card-image-container'>
+                <img src="images/neko.webp" alt="neko" />
+            </div>
+            <h1>Beer name</h1>
+            <p>Beer description</p>
+        </div>
+        <div className='shop-item-card'>
+            <div className='card-image-container'>
+                <img src="images/neko.webp" alt="neko" />
+            </div>
+            <h1>Beer name</h1>
+            <p>Beer description</p>
+        </div>
+        <div className='shop-item-card'>
+            <div className='card-image-container'>
+                <img src="images/neko.webp" alt="neko" />
+            </div>
+            <h1>Beer name</h1>
+            <p>Beer description</p>
+        </div>
     </div>
   )
 }
