e677a4f24d529f522c61c67bbfcf6b81eafdb122
KissAkos <kopyherceg@gmail.com>
2026-04-28 13:16:47 +0300

    modifying home slider to a book

--- brewing_v2/src/Routes/Home/Components/HomeBanner.jsx
+++ brewing_v2/src/Routes/Home/Components/HomeBanner.jsx
@@ -5,8 +5,8 @@ const HomeBanner = () => {
     <div className='home-banner-container'>
         <div className='home-banner'>
             <div className='banner-title'>
-                <h1>WHERE JAPANESE LEGENDS MEET CRAFT BEER.</h1>
-                <h1>HOP IN FOR A GOOD TIME!</h1>
+                <h1>UNLOCK THE SPELL OF GREAT BEER.</h1>
+                <h1>HOP IN FOR A MAGICAL TIME!</h1>
             </div>
             <div className='banner-image'>
               <div className='banner-image-container'>
--- brewing_v2/src/Routes/Home/Components/HomeSlider.jsx
+++ brewing_v2/src/Routes/Home/Components/HomeSlider.jsx
@@ -3,6 +3,7 @@ import SliderNav from './SliderNav'
 import Spinner from '../../../GeneralComponents/Spinner'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router'
+import HTMLFlipBook from 'react-pageflip';
 
 const HomeSlider = () => {
 
@@ -16,7 +17,91 @@ const HomeSlider = () => {
         beers ?
         
         <div className='slider-container'>
-            <div className={`slider-element ${beers[currentItem]?.color_scheme}`}>
+          <HTMLFlipBook 
+            width={500} 
+            height={595}
+            maxShadowOpacity={0.8}
+            drawShadow={true}
+            showCover={true}
+            flippingTime={1500}
+          >
+            <div className='book-page cover'>
+              <div className='book-img-container'>
+                <img src="wizardhops_logo.png" alt="book logo" />
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-img-container'>
+                <img src={beers[0].images[0]} alt="book-image" />
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-info'>
+                <h3>{beers[0].name}</h3>
+                <p>{beers[0].description}</p>
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-img-container'>
+                <img src={beers[1].images[0]} alt="book-image" />
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-info'>
+                <h3>{beers[1].name}</h3>
+                <p>{beers[1].description}</p>
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-img-container'>
+                <img src={beers[2].images[0]} alt="book-image" />
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-info'>
+                <h3>{beers[2].name}</h3>
+                <p>{beers[2].description}</p>
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-img-container'>
+                <img src={beers[3].images[0]} alt="book-image" />
+              </div>
+            </div>
+
+            <div className='book-page'>
+              <div className='book-info'>
+                <h3>{beers[3].name}</h3>
+                <p>{beers[3].description}</p>
+              </div>
+            </div>
+
+            <div className='book-page cover'>
+              <div className='book-img-container'>
+                <img src="wizardhops_logo.png" alt="book logo" />
+              </div>
+            </div>
+            {/* {
+              beers.map((beer) => (
+                
+                <div className='book-page'>
+                  <div className='book-img-container'>
+                    <img src={beer.images[0]} alt="book-image" />
+                  </div>
+                </div>
+              
+                
+              ))
+            } */}
+          </HTMLFlipBook>
+            {/* <div className={`slider-element ${beers[currentItem]?.color_scheme}`}>
               <div className={`slider-image ${beers[currentItem]?.color_scheme}`}>
                 <div className='slider-image-container'>
                   <img src={beers[currentItem]?.banner_img} alt="slider-image" />
@@ -30,7 +115,7 @@ const HomeSlider = () => {
                 </Link>
               </div>
             </div>
-        <SliderNav currentItem={currentItem} handleItemChangeOnClick={handleItemChangeOnClick} />
+        <SliderNav currentItem={currentItem} handleItemChangeOnClick={handleItemChangeOnClick} /> */}
         </div>
         :
         <Spinner />
